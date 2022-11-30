43d1664e5bb77a8511e99e3c70ab57cb563de0d5
Git Kurs Use <mfreitag@mt-ag.com>
2022-11-30 12:25:21 +0100

    nr1
    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import {
+   MatButtonModule,
+   MatGridListModule,
+   MatCardModule,
+  } from '@angular/material';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductComponent } from './productMask/product.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ProductOverviewComponent } from './product-overview/product-overview.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+const myRoutes: Routes = [
+  {path: 'home', component: HomepageComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'shoppingCart', component: ShoppingCartComponent},
+  {path: 'productOverview', component: ProductOverviewComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    ProductComponent,
+    ShoppingCartComponent,
+    ProductOverviewComponent,
+    ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(myRoutes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import productData from '../product.json';
+interface Product {
+  id: Number;
+  category: String;
+  productName: String;
+  price: number;   
+}
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  products: Product[] = productData;
+
+  ngOnInit(): void {}
+
+}
+
+
